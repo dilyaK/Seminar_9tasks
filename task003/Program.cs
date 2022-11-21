63fae22d1a4435480d9d00d37f499ab32476e4de
dilya <1dilnora777@gmail.com>
2022-11-21 21:30:15 +0500

    task003 added

--- task003/Program.cs
+++ task003/Program.cs
@@ -3,18 +3,24 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
-int Akkerman(int m, int n)
+
+int m = InputNumbers("Enter m: ");
+int n = InputNumbers("Enter n: ");
+
+int functionAkkerman = Ack(m, n);
+
+Console.WriteLine($" Akkerman function = {functionAkkerman} ");
+
+int Ack(int m, int n)
 {
-    if (m == 0) return n + 1;
-    else if (n == 0 && m != 0) return Akkerman(m - 1, 1);
-    else return Akkerman(m - 1, Akkerman(m, n - 1));
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
 }
-Console.WriteLine("Введите натуральное число:");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите натуральное число:");
-int n = Convert.ToInt32(Console.ReadLine());
-if (m >= 0 & n >= 0)
+
+int InputNumbers(string input) 
 {
-    Console.WriteLine($"Функция Аккермана для значений {m} и {n} равно: {Akkerman(m, n)}");
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
 }
-else Console.WriteLine("Числа должны быть неотрицательными");
